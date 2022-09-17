942517f598b5eda4d7165b503a880c8b66cfd6e3
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2022-09-17 23:56:55 +0545

    user interaction flow managed

--- calculator_1_functionalApproach/main.cpp
+++ calculator_1_functionalApproach/main.cpp
@@ -105,11 +105,12 @@ void logarithimic()
     cout << log(value) << endl;
 }
 
-void MinMaxDiff(){
+void MinMaxDiff()
+{
 
     int value_1 = 0;
     int value_2 = 0;
-    int op= 0;
+    int op = 0;
 
     cout << " [1] Minimum";
     cout << " [2] Maximum";
@@ -123,21 +124,23 @@ void MinMaxDiff(){
     cout << " Enter Second Value";
     cin >> value_2;
 
-    if( op == 1){
+    if (op == 1)
+    {
         cout << fmin(value_1, value_2);
     }
-    else if( op = 2){
+    else if (op = 2)
+    {
         cout << fdim(value_1, value_2);
     }
-    else if(op == 3){
+    else if (op == 3)
+    {
         cout << fmax(value_1, value_2);
     }
 
     cout << endl;
-
-
 }
 
+//  driver code
 int main()
 {
     int sel = 0;
@@ -146,14 +149,24 @@ int main()
 src:
 
     cout << "Advanced Calculator" << endl;
-    cout << "Written by Bhuwan Bhatt\n\n" << endl;
+    cout << "Written by Bhuwan Bhatt\n\n"
+         << endl;
 
     cout << "[1] Arithmeticn\n";
     cout << "[2] Trigonometric \n";
     cout << "[3] Exponentialn \n";
     cout << "[4] Logarithmicn \n";
     cout << "[5] MinMaxDiff \n";
-    cout << "Your choice:" << endl;
+
+    cin >> sel;
+    cout << "Your Entered choice is :" << sel << endl;
+
+    cout << " Do you want to continue this choice ? y/n";
+
+    while (choice == 'y')
+    {
+        continue;
+    }
 
     while (choice == 'y')
     {
